932f5b58bed13dcac13c328fa9dc154549f9736e
saajid-ahamed <e1398310@u.nus.edu>
2025-05-29 17:44:13 +0800

    u

--- app/loginpage.jsx
+++ app/loginpage.jsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const { user } = useUser
+  const { user } = useUser()
 
   const handleSubmit = () => {
     console.log('current user:', user)
