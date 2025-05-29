06e9b4b2d6ec7c4a81cf8ea10e7386252e95be4a
saajid-ahamed <e1398310@u.nus.edu>
2025-05-29 22:26:07 +0800

    bug

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,9 @@
 // app/(auth)/_layout.jsx
 import { Slot } from "expo-router";
 import { UserProvider } from "../../context/UserContext"; // adjust path as needed
-import { useUser } from "../../hooks/useUser";
+
 
 export default function AuthLayout() {
-
-  const { user } = useUser()
-  console.log(user)
   return (
     <UserProvider>
       <Slot />
