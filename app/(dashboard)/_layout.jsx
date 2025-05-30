6a51836a50b70051538e9be3fd1809f7ccf0a7fe
Kaung Khant Min <khantminn@192.168.1.191>
2025-05-30 21:47:14 +0800

    changed tab icons for easier identification

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,12 +1,39 @@
 import UserOnly from "../../components/auth/UserOnly";
 import { Tabs } from "expo-router"
+import { MaterialIcons } from "@expo/vector-icons"; 
 
 export default function DashboardLayout() {
   return (
     <UserOnly>
-      <Tabs
-        screenOptions={{ headerShown: false }}
-      />
+      <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs.Screen
+          name="mainpage" // This matches the file name 
+          options={{
+            title: "Home", // Custom title
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookingpage" 
+          options={{
+            title: "Bookings", // Custom title
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="event" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profilepage" 
+          options={{
+            title: "Profile", // Custom title
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
     </UserOnly>
   );
 }
--- app/(dashboard)/bookingpage.jsx
+++ app/(dashboard)/bookingpage.jsx
@@ -1,6 +1,6 @@
  import { StyleSheet, Text, View } from 'react-native'
  import React from 'react'
- 
+
 const bookingpage = () => {
   return (
     <View style = {styles.container}>
