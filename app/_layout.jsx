636aa1778d933c876571ced62404b2b2caebe793
Kaung Khant Min <khantminn@192.168.1.191>
2025-05-30 23:06:14 +0800

    hi

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,8 @@
 import { Slot } from "expo-router";
 import { UserProvider } from "../context/UserContext";
 
-
-
 export default function RootLayout() {
+
   return (
     <UserProvider>
         <Slot />
---
+++ components/ThemedLoader.jsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator, useColorScheme } from 'react-native'
+
+const ThemeLoader = () => {
+    return  (
+        <ActivityIndicator size='large' color = "black" />
+    )
+}
+
+export default ThemedLoader;
