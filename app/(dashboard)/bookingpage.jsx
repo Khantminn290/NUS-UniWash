8ace55d8bda9fabbecfd8f1aa903da6e8f057f1a
saajid-ahamed <e1398310@u.nus.edu>
2025-06-06 15:12:25 +0800

    hi

---
+++ app/(dashboard)/bookingpage.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useContext }  from 'react'
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { functions } from '../lib/appwrite';
+import { UserContext } from '../context/UserContext';
+
+const bookingpage = () => {
+  return (
+    <View>
+      <Text>bookingpage</Text>
+    </View>
+  )
+}
+
+export default bookingpage
+
+const styles = StyleSheet.create({})
